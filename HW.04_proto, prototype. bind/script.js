d4c151925a7ea17ad3d8c8ade97ea33af89f81f0
Yevhenii007 <yevhenii.zhuravskyi.xt.2014@gmail.com>
2020-06-18 20:26:29 +0300

    prototype HM adding

---
+++ HW.04_proto, prototype. bind/script.js
@@ -0,0 +1,59 @@
+function CoffeeMake() {}
+CoffeeMake.prototype.on = function () {
+	console.log("Turn on");
+}
+CoffeeMake.prototype.off = function () {
+	console.log("Turn off");
+}
+
+function DripCoffeeMaker() {}
+DripCoffeeMaker.prototype = CoffeeMake.prototype;
+
+DripCoffeeMaker.prototype.standHeating = function () {
+	console.log(`You have chosen to heat the stand`);
+}
+DripCoffeeMaker.prototype.startDelay = function () {
+	console.log(`You have selected a start delay.`);
+}
+
+
+
+
+function CarobCoffeeMaker() {}
+CarobCoffeeMaker.prototype = CoffeeMake.prototype;
+
+CarobCoffeeMaker.prototype.howMuchWatterPerServing = function (number) {
+	console.log(`You chose ${number} parts of watter per serving.`);
+}
+CarobCoffeeMaker.prototype.waterVaporSupply = function () {
+	console.log(`Water vapor is supplied.`);
+}
+
+
+
+
+function CoffeeMachine() {}
+CoffeeMachine.prototype = CarobCoffeeMaker.prototype;
+
+CoffeeMachine.prototype.grainGrinding = function () {
+	console.log(`Grain grinding is carried out.`);
+}
+CoffeeMachine.prototype.drinkSelection = function (type) {
+	console.log(`You chose a ${type} drink.`);
+}
+
+
+let cfMake = new CoffeeMake();
+let drip = new DripCoffeeMaker();
+let carob = new CarobCoffeeMaker("2");
+let machine = new CoffeeMachine("cappuccino")
+
+console.log(machine);
+
+
+cfMake.on();
+drip.startDelay();
+carob.howMuchWatterPerServing("2");
+machine.drinkSelection("cappuccino");
+machine.off();
+machine.startDelay();
