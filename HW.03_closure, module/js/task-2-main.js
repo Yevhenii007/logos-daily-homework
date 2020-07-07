851dbeb59eeae6931bbd5983db30076aa26c67a5
Yevhenii007 <yevhenii.zhuravskyi.xt.2014@gmail.com>
2020-07-08 01:24:55 +0300

    HM.11

--- HW.03_closure, module/js/task-2-main.js
+++ HW.03_closure, module/js/task-2-main.js
@@ -3,10 +3,10 @@ import * as module from "./task-2-module.js";
 let firstElem = (x) => document.querySelector(x);
 let arrayElem = (x) => document.querySelectorAll(x);
 
-firstElem(".available__balance").innerHTML = `${module.checkBalance()} грн.`
-firstElem(".available__beer").innerHTML = `${module.checkBeer()} шт.`
-firstElem(".available__vine").innerHTML = `${module.checkVine()} шт.`
-firstElem(".available__pepsi").innerHTML = `${module.checkPepsi()} шт.`
+firstElem(".available__balance").innerHTML = `${module.checkBalance()} грн.`;
+firstElem(".available__beer").innerHTML = `${module.checkBeer()} шт.`;
+firstElem(".available__vine").innerHTML = `${module.checkVine()} шт.`;
+firstElem(".available__pepsi").innerHTML = `${module.checkPepsi()} шт.`;
 
 let add = arrayElem("input[type='button']")[0];
 let buy = arrayElem("input[type='button']")[1];
@@ -14,7 +14,41 @@ let buy = arrayElem("input[type='button']")[1];
 add.addEventListener("click", function () {
 	let amount = firstElem(".buying__number");
 
-	module.orderProduct(amount.value);
+	let beerOrder = firstElem("input[value='Beer']");
+	let vineOrder = firstElem("input[value='Vine']");
+	let pepsiOrder = firstElem("input[value='Pepsi']");
+
+	function orderProduct(count) {
+		if (count < 0) {
+			alert("You can`t enter a negative value");
+		} else if (beerOrder.checked) {
+			if (module.checkBeer() < count) {
+				alert(
+					`Sorry, but there are ${module.checkBeer()} beer bottles left in the warehouse`
+				);
+			} else {
+				module.buyingOrderedBeer(count);
+			}
+		} else if (vineOrder.checked) {
+			if (module.checkVine() < count) {
+				alert(
+					`Sorry, but there are ${module.checkVine()} vine bottles left in the warehouse`
+				);
+			} else {
+				module.buyingOrderedVine(count);
+			}
+		} else if (pepsiOrder.checked) {
+			if (module.checkPepsi() < count) {
+				alert(
+					`Sorry, but there are ${module.checkPepsi()} pepsi bottles left in the warehouse`
+				);
+			} else {
+				module.buyingOrderedPepsi(count);
+			}
+		}
+	}
+
+	orderProduct(amount.value);
 
 	firstElem(".buying__number").value = "";
 });
@@ -30,9 +64,12 @@ buy.addEventListener("click", function () {
 	if (firstElem(".buying__ordered").innerHTML == "") {
 		firstElem(".information").innerHTML = "";
 	} else {
-		firstElem(".information").innerHTML += `Всього: ${module.checkOverallPrice()} гривень`;
+		firstElem(
+			".information"
+		).innerHTML += `Всього: ${module.checkOverallPrice()} гривень`;
 	}
 
-	firstElem(".buying__ordered").innerHTML = "";
+	module.zeroOverallPrice()
 
+	firstElem(".buying__ordered").innerHTML = "";
 });
--- HW.03_closure, module/js/task-2-module.js
+++ HW.03_closure, module/js/task-2-module.js
@@ -10,51 +10,33 @@ let vinePrice = 120;
 let pepsiPrice = 20;
 let overallPrice = 0;
 
-let beerOrder = firstElem("input[value='Beer']");
-let vineOrder = firstElem("input[value='Vine']");
-let pepsiOrder = firstElem("input[value='Pepsi']");
+function zeroOverallPrice() {
+	return overallPrice = 0;
+}
 
-
-function orderProduct(count) {
-	overallPrice = 0;
-	if (count < 0) {
-		alert("You can`t enter a negative value");
-	} else if (beerOrder.checked) {
-		if (beerCount < count) {
-			alert(`Sorry, but there are ${beerCount} beer bottles left in the warehouse`);
-		} else {
-			firstElem(
-				".buying__ordered"
-			).innerHTML += `<div>Пиво: ${count} шт.</div>`;
-			beerCount -= count;
-			overallPrice += beerPrice * count;
-			balance += overallPrice;
-		}
-	} else if (vineOrder.checked) {
-		if (vineCount < count) {
-			alert(`Sorry, but there are ${vineCount} vine bottles left in the warehouse`);
-		} else {
-			firstElem(
-				".buying__ordered"
-			).innerHTML += `<div>Вино: ${count} шт.</div>`;
-			vineCount -= count;
-			overallPrice += vinePrice * count;
-			balance += overallPrice;
-
-		}
-	} else if (pepsiOrder.checked) {
-		if (pepsiCount < count) {
-			alert(`Sorry, but there are ${pepsiCount} pepsi bottles left in the warehouse`);
-		} else {
-			firstElem(
-				".buying__ordered"
-			).innerHTML += `<div>Пепсі: ${count} шт.</div>`;
-			pepsiCount -= count;
-			overallPrice += pepsiPrice * count;
-			balance += overallPrice;
-
-		}
-	}
+function buyingOrderedBeer(count) {
+	firstElem(
+		".buying__ordered"
+	).innerHTML += `<div>Пиво: ${count} шт.</div>`;
+	beerCount -= count;
+	overallPrice += beerPrice * count;
+	balance += overallPrice;
+}
+function buyingOrderedVine(count) {
+	firstElem(
+		".buying__ordered"
+	).innerHTML += `<div>Вино: ${count} шт.</div>`;
+	vineCount -= count;
+	overallPrice += vinePrice * count;
+	balance += overallPrice;
+}
+function buyingOrderedPepsi(count) {
+	firstElem(
+		".buying__ordered"
+	).innerHTML += `<div>Пепсі: ${count} шт.</div>`;
+	pepsiCount -= count;
+	overallPrice += pepsiPrice * count;
+	balance += overallPrice;
 }
 
 function checkBalance() {
@@ -79,5 +61,8 @@ export {
 	checkBeer,
 	checkVine,
 	checkPepsi,
-	orderProduct,
+	buyingOrderedBeer,
+	buyingOrderedVine,
+	buyingOrderedPepsi,
+	zeroOverallPrice,
 };
