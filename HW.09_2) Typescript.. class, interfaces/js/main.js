c3a073a20da11ee3645f247577900a4ccde98b88
Yevhenii007 <yevhenii.zhuravskyi.xt.2014@gmail.com>
2020-12-26 14:10:25 +0200

    hw.09_bug_fixes

--- HW.09_2) Typescript.. class, interfaces/js/main.js
+++ HW.09_2) Typescript.. class, interfaces/js/main.js
@@ -1,111 +1,172 @@
 let addUserBtn = document.querySelector("input[value='Add user']");
 let saveEditedUserBtn = document.querySelector("input[value='Save edited user']");
+let hidenInputId = document.querySelector("#hiden-input-id");
 let loginInputValue = document.querySelectorAll("input[class='form-control'")[0];
 let passwordInputValue = document.querySelectorAll("input[class='form-control'")[1];
 let emailInputValue = document.querySelectorAll("input[class='form-control'")[2];
+let tbody = document.querySelector("tbody");
+let arrayOfTbodyTh = document.getElementsByClassName("sequence-number");
+let lastAssignedIndex = 0;
+let editableTrId;
+let numberOfPreviousThInArrayOfTbodyTh;
+let existingEmailValue = false;
+let trElement;
+let loginTdElement;
+let passwordTdElement;
+let emailTdElement;
 let editUserBtn;
 let deleteUserBtn;
-let tbody = document.querySelector("tbody");
-let arrayUsers = [];
-let userIndex;
-let lastAssignedIndex = 0;
-class Obj {
-    constructor(id, login, password, email) {
-        this.id = id;
-        this.login = login;
-        this.password = password;
-        this.email = email;
-    }
-    generateObj() {
-        return { ...this };
-    }
-}
 addUserBtn.addEventListener("click", function () {
-    addUserFunction(loginInputValue.value, passwordInputValue.value, emailInputValue.value);
+    existingEmailFunction();
+    if (existingEmailValue === false) {
+        let trId = addUserFunction(loginInputValue.value, passwordInputValue.value, emailInputValue.value);
+        addListerersToEditDeleteBtns(trId);
+    }
 });
 saveEditedUserBtn.addEventListener("click", function () {
-    saveEditedUserFunction();
-});
-function renderElementWithClickHandlers() {
-    render();
-    editUserBtn = document.querySelectorAll("input[value='Edit']");
-    deleteUserBtn = document.querySelectorAll("input[value='Delete']");
-    for (let index = 0; index < arrayUsers.length; index++) {
-        editUserBtn[index].addEventListener("click", function (e) {
-            editUserFunction(e.target.closest("tr").id);
-        });
-        deleteUserBtn[index].addEventListener("click", function (e) {
-            deleteUserFunction(e.target.closest("tr").id);
-            if (arrayUsers.length === 0) {
-                loginInputValue.value = '';
-                passwordInputValue.value = '';
-                emailInputValue.value = '';
-                addUserBtn.classList.remove("d-none");
-                saveEditedUserBtn.classList.add("d-none");
-            }
-        });
+    existingEmailFunction();
+    if (existingEmailValue === false) {
+        saveEditedUserFunction(editableTrId);
     }
+});
+function addListerersToEditDeleteBtns(trId) {
+    searchingElementsInTbody(trId);
+    editUserBtn.addEventListener("click", function (e) {
+        editUserFunction(e.target.closest("tr").id);
+    });
+    deleteUserBtn.addEventListener("click", function (e) {
+        deleteUserFunction(e.target.closest("tr").id);
+    });
 }
 function addUserFunction(log, pass, em) {
     if (loginInputValue.value && passwordInputValue.value && emailInputValue.value) {
         let obj = { id: lastAssignedIndex, login: log, password: pass, email: em };
         lastAssignedIndex++;
-        arrayUsers.push(obj);
-        loginInputValue.value = '';
-        passwordInputValue.value = '';
-        emailInputValue.value = '';
-        renderElementWithClickHandlers();
-    }
-    else {
-        alert('Please enter more data.');
-    }
-}
-function saveEditedUserFunction() {
-    let res = new Obj(arrayUsers[userIndex].id, loginInputValue.value, passwordInputValue.value, emailInputValue.value);
-    arrayUsers.splice(userIndex, 1, res.generateObj());
-    loginInputValue.value = '';
-    passwordInputValue.value = '';
-    emailInputValue.value = '';
-    addUserBtn.classList.remove("d-none");
-    saveEditedUserBtn.classList.add("d-none");
-    renderElementWithClickHandlers();
-}
-function editUserFunction(id) {
-    userIndex = getElementIndexById(Number(id));
-    loginInputValue.value = arrayUsers[userIndex].login;
-    passwordInputValue.value = arrayUsers[userIndex].password;
-    emailInputValue.value = arrayUsers[userIndex].email;
-    addUserBtn.classList.add("d-none");
-    saveEditedUserBtn.classList.remove("d-none");
-}
-function deleteUserFunction(id) {
-    let elemIndex = getElementIndexById(Number(id));
-    arrayUsers.splice(elemIndex, 1);
-    renderElementWithClickHandlers();
-}
-function getElementIndexById(id) {
-    for (let index = 0; index < arrayUsers.length; index++) {
-        if (arrayUsers[index].id === id) {
-            return index;
-        }
-    }
-    throw new Error(`NO ELEMENT BY ID ${id} FOUND`);
-}
-function render() {
-    tbody.innerHTML = '';
-    for (let i = 0; i < arrayUsers.length; i++) {
-        tbody.innerHTML += `
-    <tr id="${arrayUsers[i].id}">
-      <th scope="row">${i + 1}</th>
-      <td>${arrayUsers[i].login}</td>
-      <td>${arrayUsers[i].password}</td>
-      <td>${arrayUsers[i].email}</td>
+        let tagTr = document.createElement('tr');
+        tagTr.id = `${obj.id}`;
+        tagTr.innerHTML = `
+      <th class="sequence-number"></th>
+      <td class="login">${obj.login}</td>
+      <td class="password">${obj.password}</td>
+      <td class="email">${obj.email}</td>
       <td>
         <input type="button" class="btn btn-warning" value="Edit"/>
       </td>
       <td>
         <input type="button" class="btn btn-danger" value="Delete"/>
       </td>
-    </tr>`;
+      <td class="swich-btns"></td>`;
+        tbody.append(tagTr);
+        let arrayTdSwichBtns = document.querySelectorAll(".swich-btns");
+        if (arrayTdSwichBtns.length === 2) {
+            let tagFirstButtonDown = document.createElement('button');
+            tagFirstButtonDown.innerHTML = "<i class='fa fa-angle-down'></i>";
+            tagFirstButtonDown.style.padding = "10px 25px";
+            arrayTdSwichBtns[0].append(tagFirstButtonDown);
+            let tagLastButtonUp = document.createElement('button');
+            tagLastButtonUp.innerHTML = "<i class='fa fa-angle-up'></i>";
+            tagLastButtonUp.style.padding = "10px 25px";
+            arrayTdSwichBtns[arrayTdSwichBtns.length - 1].append(tagLastButtonUp);
+        }
+        if (arrayTdSwichBtns.length > 2) {
+            let tagTdBtns = document.createElement('td');
+            tagTdBtns.classList.add("swich-btns");
+            let tagButtonUp = document.createElement('button');
+            let tagButtonDown = document.createElement('button');
+            tagButtonUp.innerHTML = "<i class='fa fa-angle-up'></i>";
+            tagButtonDown.innerHTML = "<i class='fa fa-angle-down'></i>";
+            tagTdBtns.append(tagButtonUp);
+            tagTdBtns.append(tagButtonDown);
+            arrayTdSwichBtns[arrayTdSwichBtns.length - 2].replaceWith(tagTdBtns);
+            let tagLastButtonUp = document.createElement('button');
+            tagLastButtonUp.innerHTML = "<i class='fa fa-angle-up'></i>";
+            tagLastButtonUp.style.padding = "10px 25px";
+            arrayTdSwichBtns[arrayTdSwichBtns.length - 1].append(tagLastButtonUp);
+        }
+        searchingElementsInTbody(obj.id);
+        numberOfPreviousTh();
+        numberingOfTrElements(numberOfPreviousThInArrayOfTbodyTh);
+        deleteInputsValue();
+        return obj.id;
+    }
+    else {
+        alert('Please enter more data.');
+    }
+}
+function saveEditedUserFunction(editableTrId) {
+    searchingElementsInTbody(editableTrId);
+    loginTdElement.textContent = `${loginInputValue.value}`;
+    passwordTdElement.textContent = `${passwordInputValue.value}`;
+    emailTdElement.textContent = `${emailInputValue.value}`;
+    deleteInputsValue();
+    changeBtnToAddUserBtn();
+}
+function editUserFunction(trId) {
+    editableTrId = Number(trId);
+    searchingElementsInTbody(editableTrId);
+    hidenInputId.value = trElement.id;
+    loginInputValue.value = loginTdElement.textContent;
+    passwordInputValue.value = passwordTdElement.textContent;
+    emailInputValue.value = emailTdElement.textContent;
+    changeBtnToSaveEditedUserBtn();
+}
+function deleteUserFunction(trId) {
+    searchingElementsInTbody(Number(trId));
+    numberOfPreviousTh();
+    console.log(trElement.id);
+    trElement.parentElement.removeChild(trElement);
+    numberingOfTrElements(numberOfPreviousThInArrayOfTbodyTh);
+    if (trElement.id === hidenInputId.value) {
+        deleteInputsValue();
+        changeBtnToAddUserBtn();
+    }
+}
+function searchingElementsInTbody(trId) {
+    trElement = document.getElementById(`${trId}`);
+    loginTdElement = trElement.querySelector(".login");
+    passwordTdElement = trElement.querySelector(".password");
+    emailTdElement = trElement.querySelector(".email");
+    editUserBtn = trElement.querySelector("input[value='Edit']");
+    deleteUserBtn = trElement.querySelector("input[value='Delete']");
+}
+function numberOfPreviousTh() {
+    if (trElement.previousElementSibling !== null) {
+        numberOfPreviousThInArrayOfTbodyTh = trElement.previousElementSibling.firstElementChild.textContent;
+    }
+    else {
+        numberOfPreviousThInArrayOfTbodyTh = 1;
+    }
+}
+function numberingOfTrElements(numberOfPreviousThInArrayOfTbodyTh) {
+    for (let index = numberOfPreviousThInArrayOfTbodyTh - 1; index < arrayOfTbodyTh.length; index++) {
+        arrayOfTbodyTh[index].textContent = `${index + 1}`;
+    }
+}
+function deleteInputsValue() {
+    loginInputValue.value = '';
+    passwordInputValue.value = '';
+    emailInputValue.value = '';
+}
+function changeBtnToAddUserBtn() {
+    addUserBtn.classList.remove("d-none");
+    saveEditedUserBtn.classList.add("d-none");
+}
+function changeBtnToSaveEditedUserBtn() {
+    addUserBtn.classList.add("d-none");
+    saveEditedUserBtn.classList.remove("d-none");
+}
+function existingEmailFunction() {
+    let emailArray = document.querySelectorAll(".email");
+    if (emailArray.length !== 0) {
+        for (const emailTdElementInArray of emailArray) {
+            if (emailTdElementInArray.textContent === emailInputValue.value) {
+                alert("User with such email already exist");
+                existingEmailValue = true;
+                break;
+            }
+            else {
+                existingEmailValue = false;
+            }
+        }
     }
 }
--- HW.09_2) Typescript.. class, interfaces/ts/main.ts
+++ HW.09_2) Typescript.. class, interfaces/ts/main.ts
@@ -2,123 +2,248 @@
 let addUserBtn = document.querySelector("input[value='Add user']") as HTMLInputElement;
 let saveEditedUserBtn = document.querySelector("input[value='Save edited user']") as HTMLInputElement;
 
+let hidenInputId = document.querySelector("#hiden-input-id") as HTMLInputElement;
 let loginInputValue = document.querySelectorAll("input[class='form-control'")[0] as HTMLInputElement;
 let passwordInputValue = document.querySelectorAll("input[class='form-control'")[1] as HTMLInputElement;
 let emailInputValue = document.querySelectorAll("input[class='form-control'")[2] as HTMLInputElement;
 
-let editUserBtn: NodeListOf<Element>;
-let deleteUserBtn: NodeListOf<Element>;
-
 let tbody = document.querySelector("tbody") as unknown as HTMLTableElement;
-let arrayUsers: Array<IUser> = [];
-let userIndex: number;
-
+let arrayOfTbodyTh = document.getElementsByClassName("sequence-number");
 let lastAssignedIndex: number = 0;
+let editableTrId: number;
+let numberOfPreviousThInArrayOfTbodyTh: number;
+let existingEmailValue: boolean = false;
 
-interface IUser {
-  id: number;
-  login: string;
-  password: string;
-  email: string;
-  generateObj?(): IUser;
-}
-class Obj implements IUser {
-  constructor(public id: number, public login: string, public password: string, public email: string) { }
-  generateObj(): IUser {
-    return { ...this };
-  }
-}
+let trElement: any;
+let loginTdElement: any;
+let passwordTdElement: any;
+let emailTdElement: any;
+let editUserBtn: any;
+let deleteUserBtn: any;
 
 addUserBtn.addEventListener("click", function (): void {
-  addUserFunction(loginInputValue.value, passwordInputValue.value, emailInputValue.value);
+  existingEmailFunction();
+  if (existingEmailValue === false) {
+    let trId = addUserFunction(loginInputValue.value, passwordInputValue.value, emailInputValue.value);
+    addListerersToEditDeleteBtns(trId);
+  }
 })
 saveEditedUserBtn.addEventListener("click", function (): void {
-  saveEditedUserFunction();
-})
-function renderElementWithClickHandlers() {
-  render();
-  editUserBtn = document.querySelectorAll("input[value='Edit']");
-  deleteUserBtn = document.querySelectorAll("input[value='Delete']");
-  for (let index = 0; index < arrayUsers.length; index++) {
-    editUserBtn[index].addEventListener("click", function (e) {
-      editUserFunction(e.target.closest("tr").id);
-    })
-    deleteUserBtn[index].addEventListener("click", function (e) {
-      deleteUserFunction(e.target.closest("tr").id);
-      if (arrayUsers.length === 0) {
-        loginInputValue.value = '';
-        passwordInputValue.value = '';
-        emailInputValue.value = '';
-        addUserBtn.classList.remove("d-none");
-        saveEditedUserBtn.classList.add("d-none");
-      }
-    })
+  existingEmailFunction();
+  if (existingEmailValue === false) {
+    saveEditedUserFunction(editableTrId);
   }
+})
+function addListerersToEditDeleteBtns(trId: number): void {
+  searchingElementsInTbody(trId);
+  editUserBtn.addEventListener("click", function (e) {
+    editUserFunction(e.target.closest("tr").id);
+  });
+  deleteUserBtn.addEventListener("click", function (e) {
+    deleteUserFunction(e.target.closest("tr").id);
+  });
 }
 
-function addUserFunction(log: string, pass: string, em: string): void {
+function addUserFunction(log: string, pass: string, em: string): number {
   if (loginInputValue.value && passwordInputValue.value && emailInputValue.value) {
     let obj = { id: lastAssignedIndex, login: log, password: pass, email: em };
     lastAssignedIndex++;
-    arrayUsers.push(obj);
-    loginInputValue.value = '';
-    passwordInputValue.value = '';
-    emailInputValue.value = '';
-    renderElementWithClickHandlers();
-  }
-  else {
-    alert('Please enter more data.')
-  }
-}
-function saveEditedUserFunction(): void {
-  let res: IUser = new Obj(arrayUsers[userIndex].id, loginInputValue.value, passwordInputValue.value, emailInputValue.value);
-  arrayUsers.splice(userIndex, 1, res.generateObj());
-  loginInputValue.value = '';
-  passwordInputValue.value = '';
-  emailInputValue.value = '';
-  addUserBtn.classList.remove("d-none");
-  saveEditedUserBtn.classList.add("d-none");
-  renderElementWithClickHandlers();
-}
-function editUserFunction(id: string): void {
-  userIndex = getElementIndexById(Number(id));
-  loginInputValue.value = arrayUsers[userIndex].login;
-  passwordInputValue.value = arrayUsers[userIndex].password;
-  emailInputValue.value = arrayUsers[userIndex].email;
-  addUserBtn.classList.add("d-none")
-  saveEditedUserBtn.classList.remove("d-none")
-}
-function deleteUserFunction(id: string): void {
-  let elemIndex: number = getElementIndexById(Number(id));
-  arrayUsers.splice(elemIndex, 1);
-  renderElementWithClickHandlers();
-}
-function getElementIndexById(id: number) {
-  for (let index = 0; index < arrayUsers.length; index++) {
-    if (arrayUsers[index].id === id) {
-      return index;
-    }
-  }
-  throw new Error(`NO ELEMENT BY ID ${id} FOUND`);
-}
-function render(): void {
-  tbody.innerHTML = '';
-  for (let i: number = 0; i < arrayUsers.length; i++) {
-    tbody.innerHTML += `
-    <tr id="${arrayUsers[i].id}">
-      <th scope="row">${i + 1}</th>
-      <td>${arrayUsers[i].login}</td>
-      <td>${arrayUsers[i].password}</td>
-      <td>${arrayUsers[i].email}</td>
+    let tagTr = document.createElement('tr');
+    tagTr.id = `${obj.id}`;
+    tagTr.innerHTML = `
+      <th class="sequence-number"></th>
+      <td class="login">${obj.login}</td>
+      <td class="password">${obj.password}</td>
+      <td class="email">${obj.email}</td>
       <td>
         <input type="button" class="btn btn-warning" value="Edit"/>
       </td>
       <td>
         <input type="button" class="btn btn-danger" value="Delete"/>
       </td>
-    </tr>`;
+      <td class="swich-btns"></td>`;
+
+    tbody.append(tagTr);
+
+    let arrayTdSwichBtns = document.querySelectorAll(".swich-btns");
+
+    if (arrayTdSwichBtns.length === 2) {
+      let tagFirstButtonDown = document.createElement('button');
+      tagFirstButtonDown.innerHTML = "<i class='fa fa-angle-down'></i>";
+      tagFirstButtonDown.style.padding = "10px 25px";
+      arrayTdSwichBtns[0].append(tagFirstButtonDown);
+      let tagLastButtonUp = document.createElement('button');
+      tagLastButtonUp.innerHTML = "<i class='fa fa-angle-up'></i>";
+      tagLastButtonUp.style.padding = "10px 25px";
+      arrayTdSwichBtns[arrayTdSwichBtns.length - 1].append(tagLastButtonUp);
+    }
+
+    if (arrayTdSwichBtns.length > 2) {
+      let tagTdBtns = document.createElement('td');
+      tagTdBtns.classList.add("swich-btns")
+      let tagButtonUp = document.createElement('button');
+      let tagButtonDown = document.createElement('button');
+      tagButtonUp.innerHTML = "<i class='fa fa-angle-up'></i>";
+      tagButtonDown.innerHTML = "<i class='fa fa-angle-down'></i>";
+      tagTdBtns.append(tagButtonUp);
+      tagTdBtns.append(tagButtonDown);
+      arrayTdSwichBtns[arrayTdSwichBtns.length - 2].replaceWith(tagTdBtns);
+
+      let tagLastButtonUp = document.createElement('button');
+      tagLastButtonUp.innerHTML = "<i class='fa fa-angle-up'></i>";
+      tagLastButtonUp.style.padding = "10px 25px";
+      arrayTdSwichBtns[arrayTdSwichBtns.length - 1].append(tagLastButtonUp);
+    }
+
+    // for (let i = 0; i < arrayTdSwichBtns.length; i++) {
+    //   if (i === 0 && arrayTdSwichBtns[i].firstElementChild === null) {
+    //     let tagFirstButtonDown = document.createElement('button');
+    //     tagFirstButtonDown.innerHTML = "<i class='fa fa-angle-down'></i>";
+    //     tagFirstButtonDown.style.padding = "10px 25px";
+    //     arrayTdSwichBtns[i].append(tagFirstButtonDown);
+    //   } else if (i > 0 && i < arrayTdSwichBtns.length - 1) {
+    //     arrayTdSwichBtns[i].innerHTML = '';
+    //     let tagButtonUp = document.createElement('button');
+    //     let tagButtonDown = document.createElement('button');
+    //     tagButtonUp.innerHTML = "<i class='fa fa-angle-up'></i>";
+    //     tagButtonDown.innerHTML = "<i class='fa fa-angle-down'></i>";
+    //     arrayTdSwichBtns[i].append(tagButtonUp);
+    //     arrayTdSwichBtns[i].append(tagButtonDown);
+    //   } else if (i == arrayTdSwichBtns.length - 1) {
+    //     let tagLastButtonUp = document.createElement('button');
+    //     tagLastButtonUp.innerHTML = "<i class='fa fa-angle-up'></i>";
+    //     tagLastButtonUp.style.padding = "10px 25px";
+    //     arrayTdSwichBtns[arrayTdSwichBtns.length - 1].append(tagLastButtonUp);
+    //   }
+    // }
+
+    // let tagTd = document.createElement('td');
+    // tagTr.append(tagTd);
+
+    // let arrayTrElements = document.querySelectorAll(`tr`);
+    // if (arrayTrElements.length >= 3) {
+    //   tbody.firstElementChild.lastElementChild.append(tagFirstButtonDown);
+    //   tbody.lastElementChild.lastElementChild.append(tagLastButtonUp);
+    // }
+
+    // tagButtonUp.parentElement.removeChild(tagButtonUp);
+    // tagButtonDown.style.padding = "10px 25px";
+
+    // tagButtonDown.parentElement.removeChild(tagButtonDown);
+    // tagButtonUp.style.padding = "10px 25px";
+
+
+
+    // tagTr.append("<button><i class='fas fa-angle-up'></i></button><button><i class='fas fa-angle-down'></i></button>");
+
+    searchingElementsInTbody(obj.id);
+    numberOfPreviousTh();
+    numberingOfTrElements(numberOfPreviousThInArrayOfTbodyTh);
+    deleteInputsValue();
+    return obj.id;
+  }
+  else {
+    alert('Please enter more data.');
   }
 }
+function saveEditedUserFunction(editableTrId: number): void {
+  searchingElementsInTbody(editableTrId);
+  loginTdElement.textContent = `${loginInputValue.value}`;
+  passwordTdElement.textContent = `${passwordInputValue.value}`;
+  emailTdElement.textContent = `${emailInputValue.value}`;
+  deleteInputsValue();
+  changeBtnToAddUserBtn();
+}
+function editUserFunction(trId: string): void {
+  editableTrId = Number(trId);
+  searchingElementsInTbody(editableTrId);
+  hidenInputId.value = trElement.id;
+  loginInputValue.value = loginTdElement.textContent;
+  passwordInputValue.value = passwordTdElement.textContent;
+  emailInputValue.value = emailTdElement.textContent;
+  changeBtnToSaveEditedUserBtn();
+}
+function deleteUserFunction(trId: string): void {
+  searchingElementsInTbody(Number(trId));
+  numberOfPreviousTh();
+  console.log(trElement.id);
+
+  trElement.parentElement.removeChild(trElement);
+  numberingOfTrElements(numberOfPreviousThInArrayOfTbodyTh);
+  if (trElement.id === hidenInputId.value) {
+    deleteInputsValue();
+    changeBtnToAddUserBtn();
+  }
+}
+
+function searchingElementsInTbody(trId: number) {
+  trElement = document.getElementById(`${trId}`);
+  loginTdElement = trElement.querySelector(".login");
+  passwordTdElement = trElement.querySelector(".password");
+  emailTdElement = trElement.querySelector(".email");
+  editUserBtn = trElement.querySelector("input[value='Edit']");
+  deleteUserBtn = trElement.querySelector("input[value='Delete']");
+}
+function numberOfPreviousTh() {
+  if (trElement.previousElementSibling !== null) {
+    numberOfPreviousThInArrayOfTbodyTh = trElement.previousElementSibling.firstElementChild.textContent;
+  } else {
+    numberOfPreviousThInArrayOfTbodyTh = 1;
+  }
+}
+function numberingOfTrElements(numberOfPreviousThInArrayOfTbodyTh: number) {
+  for (let index = numberOfPreviousThInArrayOfTbodyTh - 1; index < arrayOfTbodyTh.length; index++) {
+    arrayOfTbodyTh[index].textContent = `${index + 1}`;
+  }
+}
+function deleteInputsValue() {
+  loginInputValue.value = '';
+  passwordInputValue.value = '';
+  emailInputValue.value = '';
+}
+function changeBtnToAddUserBtn() {
+  addUserBtn.classList.remove("d-none");
+  saveEditedUserBtn.classList.add("d-none");
+}
+function changeBtnToSaveEditedUserBtn() {
+  addUserBtn.classList.add("d-none");
+  saveEditedUserBtn.classList.remove("d-none");
+}
+function existingEmailFunction() {
+  let emailArray = document.querySelectorAll(".email");
+  if (emailArray.length !== 0) {
+    for (const emailTdElementInArray of emailArray) {
+      if (emailTdElementInArray.textContent === emailInputValue.value) {
+        alert("User with such email already exist");
+        existingEmailValue = true;
+        break;
+      } else {
+        existingEmailValue = false;
+      }
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -139,7 +264,6 @@ function render(): void {
 // }
 
 ////// ////////////////////////////////////////////////////////////////
-
 
 // tbody.addEventListener("click", function (e: MouseEvent): void {
 //   deleteUser(e);
