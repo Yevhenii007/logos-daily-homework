a652d04b96a7a31fabbd538b3987d3ceae097f38
Yevhenii007 <yevhenii.zhuravskyi.xt.2014@gmail.com>
2021-01-04 14:09:22 +0200

    commit HW.13 connect parent comp with child comp

--- Ang10/src/app/app.module.ts
+++ Ang10/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Hw11Component } from './hw11/hw11.component';
 import { Hw12Component } from './hw12/hw12.component';
+import { Hw13Component } from './hw13/hw13.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Hw11Component,
     Hw12Component,
+    Hw13Component,
   ],
   imports: [
     BrowserModule,
---
+++ Ang10/src/app/hw13/hw13.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Hw13Component } from './hw13.component';
+
+describe('Hw13Component', () => {
+  let component: Hw13Component;
+  let fixture: ComponentFixture<Hw13Component>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ Hw13Component ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Hw13Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
